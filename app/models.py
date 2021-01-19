b0597d81513cd956250bb4204e1e5b5ca2dab75a
Luis Contreras <luis.e.contreras1607@gmail.com>
2021-01-19 18:39:26 -0300

    Pensando la base de datos

--- app/models.py
+++ app/models.py
@@ -6,11 +6,12 @@ from django.db import models
 
 #class producto(models.Model):
  #   name = models.CharField(verbose_name='Nombre' max_length=30)
-  #  tipo_prod = models.CharField('Tipo',max_length=10)#,choices=tipo_prod_CHOISES)    
-   # description = models.CharField(max_length=50)
-    #price = models.CharField(max_length=50)
+  #  ubicacion_actual = models.CharField('Tipo',max_length=15)
+   # ubicacion_anterior = models.CharField('Tipo',max_length=15)
+   # descripcion = models.CharField(max_length=50)
+    #cantidad = models.IntegerField()    
     #imagen = models.ImageField(upload_to="productos", null = True)
 
 
- #   def __str__(self):
-  #      return self.name + ' ' + self.tipo_prod 
+    #def __str__(self):
+    #    return self.name + ' ' + self.tipo_descripcion + ' ' + self.ubicacion_actual + ' ' + self.cantidad
